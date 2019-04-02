c6f574e9d0c2ea9009642bd6ee38800ed52123dc
Marcelo <mwbitt@gmail.com>
2019-04-02 19:13:32 -0300

    item/index change

--- layoutExamples/src/components/ListItem/Item/index.js
+++ layoutExamples/src/components/ListItem/Item/index.js
@@ -1,20 +1,20 @@
-import React from 'react'; 
-import { Alert, Text, View, TouchableOpacity, Image } from 'react-native';
-import { styles } from './styles'
+import React from "react";
+import { Alert, Text, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "./styles";
 
-export default Item = (props) => {
+export default (Item = props => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => Alert.alert(props.name)}>
-      <View style = {{flexDirection: 'row', alignItems: 'center'}}> 
-      <Image
-          style={{width: 50, height: 50,margin: 10}}
-          source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
-          borderRadius={50}
-        />
-        <Text style={styles.textItem} >{props.name}</Text>
-      </View>  
+      <TouchableOpacity onPress={() => Alert.alert(props.img)}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            style={{ width: 50, height: 50, margin: 10 }}
+            source={props.img}
+            borderRadius={50}
+          />
+          <Text style={styles.textItem}>{props.name}</Text>
+        </View>
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+});
